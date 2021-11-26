d3cc55fb5d12a45ed759f2c73e49775b7c41c7fe
Janipalen <80848618+Janipalen@users.noreply.github.com>
2021-11-26 20:02:56 +0100

    Update token-join.py

--- token-join.py
+++ token-join.py
@@ -1,1 +1,19 @@
+# We do not take any responsibility for your actions!
+# This is only made for education purposes
 
+import requests
+import json
+import time
+
+class main():
+    def __init__(self) -> None:
+        self.tokens = ["PASTE_TOKEN_HERE"]
+        self.message_url = f"https://discord.com/api/v8/channels/id/messages"
+
+    def join_server(self, link) -> None:
+        for token in self.tokens:
+            r = requests.post(url=link, headers={"authorization": token})                    
+            print(r.content.decode())
+ 
+m = main()
+m.join_server(link="https://discord.com/api/v9/invites/PASTE_LINK_HERE")
